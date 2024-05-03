b1ff328a366f5978dae8562b40a00b00f17c413d
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-05-03 16:38:34 +0200

    registered data context with DI

--- backend/MyTaskBoard.Api/Program.cs
+++ backend/MyTaskBoard.Api/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using MyTaskBoard.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("Postgres");
 
+builder.Services.AddDbContextPool<DataContext>(options =>
+{
+    options.UseNpgsql(connectionString,
+        b => b.MigrationsAssembly("MyTaskBoard.Infrastructure"));
+});
+builder.Services.AddPooledDbContextFactory<DataContext>(options =>
+{
+    options.UseNpgsql(connectionString,
+        b => b.MigrationsAssembly("MyTaskBoard.Infrastructure"));
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
