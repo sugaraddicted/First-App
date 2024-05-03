ba797506dc41c73f8ee1df6901607e379fcd8deb
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-05-03 20:06:34 +0200

    add board list repository

---
+++ backend/MyTaskBoard.Infrastructure/Repository/BoardListRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyTaskBoard.Core.Entity;
+using MyTaskBoard.Infrastructure.Persistence;
+using MyTaskBoard.Infrastructure.Repository.Base;
+using MyTaskBoard.Infrastructure.Repository.Interfaces;
+
+namespace MyTaskBoard.Infrastructure.Repository
+{
+    public class BoardListRepository : EntityBaseRepository<BoardList>, IBoardListRepository 
+    {
+        public BoardListRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
