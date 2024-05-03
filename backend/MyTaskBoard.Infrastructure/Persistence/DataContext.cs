0de590bb900ae3900588e17bb4a4303c6ac370c5
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-05-03 15:21:25 +0200

    add db context

---
+++ backend/MyTaskBoard.Infrastructure/Persistence/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyTaskBoard.Core.Entity;
+using MyTaskBoard.Core.Enums;
+
+namespace MyTaskBoard.Infrastructure.Persistence
+{
+    public class DataContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresEnum<Priority>();
+
+            modelBuilder.Entity<User>()
+                .HasBaseType<IdentityUser<Guid>>();
+
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().HasKey(x => x.UserId);
+            modelBuilder.Entity<IdentityUserRole<Guid>>().HasKey(x => x.UserId);
+            modelBuilder.Entity<IdentityUserToken<Guid>>().HasKey(x => x.UserId);
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BoardList> BoardLists { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+    }
+}
