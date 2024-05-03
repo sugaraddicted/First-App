23759a7230c72277c91396e2602270ef8d9dd501
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-05-03 21:26:03 +0200

    add list dto

---
+++ backend/MyTaskBoard.Api/Dto/BoardListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MyTaskBoard.Api.Dto
+{
+    public class BoardListDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<CardDto> Cards { get; set; }
+    }
+}
