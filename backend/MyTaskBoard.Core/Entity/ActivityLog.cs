afc44c18ff62b59b23f04e8e4dc38390195f881f
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-05-04 19:17:45 +0200

    update activityLog entity

--- backend/MyTaskBoard.Core/Entity/ActivityLog.cs
+++ backend/MyTaskBoard.Core/Entity/ActivityLog.cs
@@ -9,6 +9,8 @@ namespace MyTaskBoard.Core.Entity
         public Guid Id { get; set; }
         public string Action { get; set; }
         public string CardName { get; set; }
+        public string Before { get; set; }
+        public string After { get; set; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime Timestamp { get; set; }
