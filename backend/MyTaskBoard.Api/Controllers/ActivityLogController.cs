103890d6bc3c1065763aeff32de7ff6f0734e041
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-05-04 19:57:13 +0200

    add activity log controller

---
+++ backend/MyTaskBoard.Api/Controllers/ActivityLogController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTaskBoard.Infrastructure.Repository.Interfaces;
+
+namespace MyTaskBoard.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivityLogController : ControllerBase
+    {
+        private readonly IActivityLogRepository _activityLogRepository;
+
+        public ActivityLogController(IActivityLogRepository activityLogRepository)
+        {
+            _activityLogRepository = activityLogRepository;
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetByUserId(Guid userId)
+        {
+            var activityLogs = await _activityLogRepository.GetByUserIdAsync(userId);
+            return Ok(activityLogs);
+        }
+
+        [HttpGet("card/{cardId}")]
+        public async Task<IActionResult> GetByCardId(Guid cardId)
+        {
+            var activityLogs = await _activityLogRepository.GetByCardIdAsync(cardId);
+            return Ok(activityLogs);
+        }
+    }
+}
