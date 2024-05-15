e0dece1f04050486d697a0cd9943101acffb5ab9
Maria Prylutska <63318704+sugaraddicted@users.noreply.github.com>
2024-05-15 12:56:47 +0200

    add web application factory

---
+++ backend/MyTaskBoard.IntegrationTests/MyTaskBoardWebApplicationFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyTaskBoard.Infrastructure.Persistence;
+
+namespace MyTaskBoard.IntegrationTests
+{
+    public class MyTaskBoardWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<DataContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<DataContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+ 
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<DataContext>();
+
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
